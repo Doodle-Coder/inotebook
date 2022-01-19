489a392e860364d40ee92218230954d5863a9db6
Doodle-Coder <ayushrautela007@gmail.com>
2022-01-19 20:40:39 +0530

    backend Completed Frontend Started

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -40,4 +40,60 @@ router.post("/addnote",fetchuser,[body('title','Enter a Valid Title!!').isLength
    
 })
 
+// ROUTE 3:  Update an existing Note using: POST "api/notes/updatenote"  Login Required
+
+router.put("/updatenote/:id",fetchuser,async(req,res)=>{
+    const {title,description,tag} = req.body;
+    try{
+     
+     // if there are errors,returns bad request and the errors 
+     const newNote = {};
+     if(title){newNote.title = title};
+     if(description){newNote.description = description};
+     if(tag){newNote.tag = tag};
+
+     // Find the note to be updated
+     let note = await Notes.findById(req.params.id);
+     if(!note){
+         return res.status(404).send("Not Found");
+     }
+     if(note.user.toString() !== req.user.id) {
+         return res.status(401).send("Not Allowed");
+     }
+     note = await Notes.findByIdAndUpdate(req.params.id,{$set: newNote},{new:true})
+     res.json({note});
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("some error occurred");
+    }
+})
+
+// ROUTE 4: Delete an existing note using: DELETE "api/notes/deletenote".  Login REequired
+
+
+router.delete("/deletenote/:id",fetchuser,async(req,res)=>{
+    try{
+ 
+    
+     // Find the note to be Deleted and Delete it
+
+     let note = await Notes.findById(req.params.id);
+     if(!note){
+         return res.status(404).send("Not Found");
+     }
+
+    //  Allowed deletion only if user owns this Note
+
+     if(note.user.toString() !== req.user.id) {
+         return res.status(401).send("Not Allowed");
+     }
+     note = await Notes.findByIdAndDelete(req.params.id)
+     res.json({"Success":"Note has been deleted"});
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("some error occurred");
+    }
+})
+
+
 module.exports = router;
